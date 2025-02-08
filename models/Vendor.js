5e05621dc392cc20bba0bda0a264069a200a550e
mad674 <144526214+mad674@users.noreply.github.com>
2025-02-08 22:15:45 +0530

    Update Vendor.js

--- models/Vendor.js
+++ models/Vendor.js
@@ -3,12 +3,13 @@ const mongoose=require('mongoose');
 const vendorSchema=new mongoose.Schema({
     username:{
         type:String,
-        // required:true,
-        unique:true
+        required:true,
+        // unique:true
     },
     email:{
         type:String,
-        required:true
+        required:true,
+        unique:true
     },
     password:{
         type:String,
